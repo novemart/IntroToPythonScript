82afa8512f64dd4cd6d17fa9c5853bc4f6dec272
Seyram Amoako <seyram12@hotmail.com>
2022-02-16 10:17:13 +0000

    added a .js file

---
+++ experiment.js
@@ -0,0 +1,15 @@
+
+    
+    document.getElementById('change').onclick = function() {
+            document.getElementById('grabParagraph').innerHTML = 'Great to Hear!'
+
+        } ;
+
+    document.getElementById('change2').onclick = function() {
+            document.getElementById('grabParagraph').innerHTML = 'Hope it does get better!'
+
+    }
+    
+
+// The onclick property only accepts a function, not a string.
+
